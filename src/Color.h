2b98593136ebb4f2386a331614e484a19aba5d51
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2020-01-04 15:46:35 -0600

    deleted superfluous access qualifier

--- src/Color.h
+++ src/Color.h
@@ -6,7 +6,6 @@ namespace djf_3d {
 values. They are stored as uint8_t because they should be
 in the range 0..256. */
 struct Color {
-public:
     uint8_t red;
     uint8_t green;
     uint8_t blue;
